885b13d29d203811f44d934a0f84a02ca87e969c
Daniel Dormin <danielpinhalfilho@yahoo.com.br>
2020-09-28 05:48:21 -0300

    projeto de codigo python pronto

--- 1-semestre/linguagem-de-programacao/listaTarefa.py
+++ 1-semestre/linguagem-de-programacao/listaTarefa.py
@@ -1,21 +1,40 @@
-opcao = ["0- para ver as opcoes", "1- ver a lista", "2- adicionar item a lista", "3- remover item da lista", "4- sair"]
+# projeto feito em 26/09/2020
+opcao = ["0- para ver as opcoes", "1- ver a lista de tarefas", "2- adicionar item a lista de tarefas", "3- remover item da lista de tarefas", "4- sair"]
 opcaoDigitada = 0
+listaTarefas = []
+
 
 def escolher(num):
     if num == 0:
-      for i in opcao:
+        print("---------------------------------------------")
+        for i in opcao:
             print(i)
     elif num == 1:
-        print("opcao 1")
+        print("---------------------------------------------")
+        count = 0
+        for i in listaTarefas:
+            print(count, "-", i)
+            count = count + 1
     elif num == 2:
-        print("opcao 2")
+        print("---------------------------------------------")
+        print("Para voltar e escolher uma opção digite 0!!!")
+        add = True
+        while add:
+            valor = input("Digite uma tarefa: ")
+            if valor != '0': 
+                listaTarefas.append(valor)
+            else:
+                add = False
     elif num == 3:
-        print("opcao 3")
+        print("---------------------------------------------")
+        remover = int(input("Digite o numer de qual tarefa deseja remover: "))
+        del listaTarefas[remover]
 
 while True:
     if opcaoDigitada != 4:
         escolher(opcaoDigitada)
-        opcaoDigitada = int(input("digite a opcao: "))
+        print("___________")
+        opcaoDigitada = int(input("Digite a opcao: "))
     else:
-        print("Esse programa foi desenvolvido para o curso de sistemas de informacao 1 semestre. By Daniel Pinhal Filho")
+        print("Esse programa foi desenvolvido para o curso \nSistemas de informacao 1 semestre. \nBy Daniel Pinhal Filho")
         break
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Anhanguera
+# Pasta de projetos
+## Projetos e códigos feitos para o curso de Análise e desenvolvimento de sistemas para a faculdade Anhanguera
+
+Todos os projetos estão separados por semestre e por matéria.
